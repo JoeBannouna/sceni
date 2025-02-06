1db619797206217d22e6cd13106a3ea561a4f712
JoeBannouna <yo.m.hsm3@gmail.com>
2025-02-06 10:34:29 +1030

    MuFinder: getResultImage, plotSkewnessVals functuions

--- astrosceni/mufinder.py
+++ astrosceni/mufinder.py
@@ -3,8 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from astrosceni.image import Image
 
-class MuFinder():
-
+class MuFinder:
     def __init__(self, narrow_band_image, broad_band_image, mu_range=None, mu_resolution=0.1):
         # Class instance state
         self.narrow_band_image = narrow_band_image
@@ -51,78 +50,23 @@ class MuFinder():
         self.mu_linspace = np.linspace(start, end, int((end - start)/self.mu_resolution))
         self.is_paramaters_changed = True   # Flag that the parameters have changes
 
-    def getSkewnessVals():
+    def getSkewnessVals(self):
         if (self.is_paramaters_changed):
             self.skewness_vals = []
-            for mu in scale_factor_linspace:
-                image_data = self.narrow_band_image.getImageData() - mu*self.broad_band_image.getImageData()
-                data = np.array(image_data.flatten())
+            for mu in self.mu_linspace:
+                data = (self.narrow_band_image.getImageData() - mu*self.broad_band_image.getImageData()).flatten()
                 skews.append(np.sum(((data - np.mean(data)) / np.std(data, ddof=1)) ** 3) / (len(data) - 1))
-
         return self.skewness_vals
 
-    def plotSkewnessVals():
-        plt.plot(self.factors, self.getSkewnessVals())
-
-        # Set axis labels, anything in between $^\$ will be displayed as a mathematical symbol
+    def plotSkewnessVals(self):
         plt.xlabel('$^\mu$')
         plt.ylabel('s($^\mu$)')
-        
+        plt.plot(self.factors, self.getSkewnessVals())
 
-    # def (self):
-        
+    def getOptimalMu(self):
+        skews = self.getSkewnessVals()
+        # BSPLINE CODE
 
-
-    # def plotPixelDistribution(self, scale_factor = 0.1, nbins = 301, lower_lim = -45000.0, upper_lim = 45000.0):
-    #     """
-    #     Plots a histogram of the pixel distribution for a specific scale factor.
-
-    #     Parameters
-    #     ----------
-    #     scale_factor: float
-    #         The scale factor to consider.
-    #         Optional input.
-    #         Default value is 0.1.
-        
-    #     nbins: integer
-    #         Number of bins for the histogram.
-    #         Optional input.
-    #         Default value is 301.
-        
-    #     lower_lim: int, float
-    #         Lower limit for the range of the x-axis for the histogram.
-        
-    #     upper_lim: int, float
-    #         Upper limit for the range of the x-axis for the histogram.
-    #     """
-
-    #     self.residual_image = Subtractor.getSubtractedImage(self.NB_arr, self.BB_arr, scale_factor)
-
-    #     # Flatten the array for histogram plotting
-    #     flat_residual = self.residual_image.getImageData().flatten()
-
-    #     # Plot the histogram
-    #     plt.hist(flat_residual, bins = nbins, range = (lower_lim, upper_lim))
-
-    #     plt.text(0.5 * upper_lim, 0.9 * plt.ylim()[1], f"mu = {scale_factor}", ha='left', va='top')
-    #     plt.text(0.5 * upper_lim, 0.4 * plt.ylim()[1], f"skew = {Subtractor.skewnessCalculator(self.residual_image.getImageData()): .2f}", ha='left', va='top')
-    #     plt.yscale('log')
-
-    #     # Label axes and display the plot
-    #     plt.xlabel("Pixel Value")
-    #     plt.ylabel("Frequency")
-    #     plt.title("Pixel Distribution")
-    #     plt.show()
-
-
-    # def plotSkewVsMu(self):
-    #     plt.plot(self.factors, self.skewness_vals)
-
-    #     # Set axis labels, anything in between $^\$ will be displayed as a mathematical symbol
-    #     plt.xlabel('$^\mu$')
-    #     plt.ylabel('s($^\mu$)')
-
-    
-    # def getResultImage(self):
-    #     # Turn Residual array back into a image class.
-    #     pass
+    def getResultImage(self):
+        optimal_mu = self.getOptimalMu()
+        return Image.subtract(self.narrow_band_image, self.broad_band_image, optimal_mu)
